072a0593dd6921a028c2ce939f44b035ea4bd982
Jayanth-YVS-31 <yvsjayanth31@gmail.com>
2024-11-09 23:13:00 +0530

    url correcttion

--- application.py
+++ application.py
@@ -6,27 +6,54 @@ app = Flask(__name__)
 
 meta_model_3 = joblib.load('models/meta_model_3.pkl')
 tuned_meta_model_2 = joblib.load('models/tuned_meta_model_2.pkl')
-@app.route('/')
-def home():
-    return "Premier League Match Prediction API is running"
 
-@app.route('/predict', methods=['POST'])
+@app.route('/') 
+def home(): 
+    return "Premier League Match Prediction API is running  use https://premier-league-match-result-prediction.onrender.com/predict" 
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     try:
-        data = request.json
+        # For GET requests, we'll retrieve data from query parameters
+        if request.method == 'GET':
+            week = request.args.get('week')
+            home_team = request.args.get('homeTeam')
+            away_team = request.args.get('awayTeam')
+            lastHomeResult = int(request.args.get('lastHomeResult'))
+            lastAwayResult = int(request.args.get('lastAwayResult'))
+            homeStreak = int(request.args.get('homeStreak'))
+            homeRestDays = int(request.args.get('homeRestDays'))
+            awayStreak = int(request.args.get('awayStreak'))
+            awayRestDays = int(request.args.get('awayRestDays'))
         
-        week = data.get('week')
-        home_team = data.get('homeTeam')
-        away_team = data.get('awayTeam')
-        lastHomeResult = data.get('lastHomeResult')
-        lastAwayResult = data.get('lastAwayResult')
-        homeStreak = data.get('homeStreak')
-        homeRestDays = data.get('homeRestDays')
-        awayStreak = data.get('awayStreak')
-        awayRestDays = data.get('awayRestDays')
+        # For POST requests, we'll retrieve data from JSON body
+        elif request.method == 'POST':
+            data = request.json
+            week = data.get('week')
+            home_team = data.get('homeTeam')
+            away_team = data.get('awayTeam')
+            lastHomeResult = data.get('lastHomeResult')
+            lastAwayResult = data.get('lastAwayResult')
+            homeStreak = data.get('homeStreak')
+            homeRestDays = data.get('homeRestDays')
+            awayStreak = data.get('awayStreak')
+            awayRestDays = data.get('awayRestDays')
 
         week_mapping = {'Early': 0, 'Mid': 1, 'End': 2}
-        home_team_mapping = { ... }  
+        home_team_mapping = {
+    'Coventry City': 0, 'Leeds United': 1, 'Sheffield Utd': 2, 'Crystal Palace': 3,
+    'Arsenal': 4, 'Ipswich Town': 5, 'Everton': 6, 'Southampton': 7, 'Chelsea': 8,
+    "Nott'ham Forest": 9, 'Manchester City': 10, 'Blackburn': 11, 'Wimbledon': 12,
+    'Tottenham': 13, 'Liverpool': 14, 'Aston Villa': 15, 'Oldham Athletic': 16,
+    'Middlesbrough': 17, 'Norwich City': 18, 'QPR': 19, 'Manchester Utd': 20,
+    'Sheffield Weds': 21, 'Newcastle Utd': 22, 'West Ham': 23, 'Swindon Town': 24,
+    'Leicester City': 25, 'Bolton': 26, 'Sunderland': 27, 'Derby County': 28,
+    'Barnsley': 29, 'Charlton Ath': 30, 'Watford': 31, 'Bradford City': 32, 'Fulham': 33,
+    'West Brom': 34, 'Birmingham City': 35, 'Wolves': 36, 'Portsmouth': 37,
+    'Wigan Athletic': 38, 'Reading': 39, 'Stoke City': 40, 'Hull City': 41, 'Burnley': 42,
+    'Blackpool': 43, 'Swansea City': 44, 'Cardiff City': 45, 'Bournemouth': 46,
+    'Huddersfield': 47, 'Brighton': 48, 'Brentford': 49
+}  # Add your home team mapping here
 
         season_phase = week_mapping.get(week, -1)
 
@@ -57,5 +84,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     app.run(debug=True)
